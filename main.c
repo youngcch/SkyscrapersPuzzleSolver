ed989447993042ce89508bd52c079c1356308b06
youngcch <cyc8649@gmail.com>
2023-06-10 07:49:15 +0900

    Create main file and add guide text

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	main()
+{
+	//안내 문구
+	printf("Insert in the order of top, bottom, left, right.\n");	
+	printf("(top and bottom values are left to right, left and right values are top to bottom)\n");
+	printf("ex: 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4\n");
+}
+
