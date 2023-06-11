3af142ba1b698c30132342b1adcc67dc919eb175
youngcch2 <cyc8649+2@gmail.com>
2023-06-11 10:33:56 +0900

    Add Function to check values ​​in downward direction

--- check_answer.c
+++ check_answer.c
@@ -26,3 +26,32 @@ int	check_up(char arr[4][5], char up[4])
 	}
 	return (1);
 }
+
+int	check_down(char arr[4][5], char down[4])
+{
+	char	max;
+	int		i;
+	int		cnt;
+	int		j;
+
+	i = 0;
+	while (i < 4)
+	{
+		max = arr[3][i];
+		cnt = 1;
+		j = 3;
+		while (j > 0)
+		{
+			if (max < arr[j - 1][i])
+			{
+				max = arr[j - 1][i];
+				cnt ++;
+			}
+			j--;
+		}
+		if (down[i] != '0' + cnt)
+			return (0);
+		i ++;
+	}
+	return (1);
+}
