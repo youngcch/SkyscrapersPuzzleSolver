2e75bf064276588895c53847edbb61999838530c
youngcch2 <cyc8649+2@gmail.com>
2023-06-11 10:34:14 +0900

    Add Function to check values ​​in left direction

--- check_answer.c
+++ check_answer.c
@@ -55,3 +55,32 @@ int	check_down(char arr[4][5], char down[4])
 	}
 	return (1);
 }
+
+int	check_left(char arr[4][5], char left[4])
+{
+	char	max;
+	int		i;
+	int		cnt;
+	int		j;
+
+	i = 0;
+	while (i < 4)
+	{
+		max = arr[i][0];
+		cnt = 1;
+		j = 0;
+		while (j < 3)
+		{
+			if (max < arr[i][j + 1])
+			{
+				max = arr[i][j + 1];
+				cnt ++;
+			}
+			j++;
+		}
+		if (left[i] != '0' + cnt)
+			return (0);
+		i ++;
+	}
+	return (1);
+}
